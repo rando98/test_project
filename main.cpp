ad08f3a6636bcbb797cc2626a92bd23c7e810bc2
Randeep Singh <randeep.singh@mail.polimi.it>
2021-08-08 20:46:42 +0000

    Create new instance of MyCLass and include vector

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include "Myclass.hpp"
 
+#include <vector>
+
 int
 main(int argc, char *argv[])
 {
     MyClass m;
+    MyCLass m2;
+
     std::cout << "New line" << std::endl;
 
     return 0;
